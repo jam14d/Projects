abc499f110d8c879508094a143b9fc2d8d26fcc1
jmortelrb <jmortel@revealbio.com>
2025-02-12 15:06:20 -0800

    covid reference and variance genome lengths

--- Bioinformatics/covid_analysis/scripts/sars_cov2_mutation_finder.py
+++ Bioinformatics/covid_analysis/scripts/sars_cov2_mutation_finder.py
@@ -1,9 +1,19 @@
-
-
+from Bio import SeqIO
 from Bio.Seq import Seq
 
-dna = Seq("ATGCGTACGTAGCTAGCTAG")
-print("Reverse Complement:", dna.reverse_complement())
-print("mRNA:", dna.transcribe())
-print("Protein:", dna.translate())
+# Load reference genome
+ref_file = "/Users/jamieannemortel/Projects/Bioinformatics/covid_analysis/data/reference-NC_045512.fasta"
 
+# Load variant (an omnicron)
+variant_file = "/Users/jamieannemortel/Projects/Bioinformatics/covid_analysis/data/BA.3.1.fasta"
+
+def load_fasta(filename):
+    record = SeqIO.read(filename, "fasta")
+    return str(record.seq)
+
+# Load sequences
+reference_seq = load_fasta(ref_file)
+variant_seq = load_fasta(variant_file)
+
+print(f"Reference Genome Length: {len(reference_seq)}")
+print(f"Variant Genome Length: {len(variant_seq)}")
